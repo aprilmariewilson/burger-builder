04671980d773d09a67488e43ea9550026de32680
aprilmariewilson <aprilmariewilson@yahoo.com>
2019-05-13 12:15:40 -0600

    navbar added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Auxilary';
 import './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const Layout = (props) => (
     <Aux>
-        <div>Toolbar, SideDrawer, BackDrop</div>
+       <Toolbar />
         <main className='Content'>
             {props.children}
         </main>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -85,7 +85,8 @@ class BurgerBuilder extends Component {
                     <OrderSummery 
                     purchaseContinued={this.purchaseContinueHandler}
                     purchaseCancelled={this.purchaseCancelHandler}
-                    ingredients= {this.state.ingredients}/>
+                    ingredients= {this.state.ingredients}
+                    price ={this.state.totalPrice}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
